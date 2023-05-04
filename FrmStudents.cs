e5b4541fed6df0b1bc1ae7ba58a1b83d81b0b8b2
Vedran Draganić <vdraganic21@foi.hr>
2023-05-04 22:14:55 +0200

    Add project files.

---
+++ FrmStudents.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evaluation_Manager
+{
+    public partial class FrmStudents : Form
+    {
+        public FrmStudents()
+        {
+            InitializeComponent();
+        }
+    }
+}
